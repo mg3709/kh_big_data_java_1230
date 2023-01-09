523f7dfd7ef9e158e6795d91194b96fdd3f0f704
kmg0205 <mg3709@naver.com>
2023-01-09 11:24:53 +0900

    yes

---
+++ work_java/Java_Big_Data_1230/src/중첩어레이/DoubleArrayEx.java
@@ -0,0 +1,24 @@
+package 중첩어레이;
+//2차원 배열에 대해서
+public class DoubleArrayEx {
+    public static void main(String[] args) {
+        int[][] arr = new int[2][3];
+        int k = 10;
+
+        for(int i = 0; i < arr.length; i++){//행의 갯수를 알수 있음
+            for(int j = 0; j < arr[i].length; j++){
+
+                arr[i][j] = k;
+                k += 10;
+            }
+
+        }
+
+for(int[] row : arr){
+    for(int e: row)System.out.print(e + " ");
+    System.out.println();
+    
+       }
+
+    }
+}
