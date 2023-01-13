4008bfa77eeaeedfdf6eb3e29a6622f77c5b6a43
kmg0205 <mg3709@naver.com>
2023-01-13 14:11:25 +0900

    yes

---
+++ work_java/Java_Big_Data_1230/src/객체타입확인/InstanceOf.java
@@ -0,0 +1,34 @@
+package 객체타입확인;
+
+//instanceof : 객체 타입 확인 연산자, 부모와 지식 관계 성립 여부 확인
+public class InstanceOf {
+    public static void main(String[] args) {
+
+        Parent parent = new Parent("부모");
+        Child child = new Child("자식");
+
+        System.out.println(parent instanceof  Parent);//True
+        System.out.println(child instanceof  Parent);//True
+        System.out.println(parent instanceof  Child);//False
+        System.out.println(child instanceof  Child);//True
+
+
+    }
+}
+
+class Parent{
+    String name;
+    public Parent(String name){
+        this.name = name;
+    }
+
+}
+
+class Child extends Parent{
+    public Child(String name){
+        super(name);
+        this.name = name;
+
+    }
+
+}
