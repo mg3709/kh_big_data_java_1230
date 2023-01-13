e15dc3293f7ce80a710c2dd651c082adc6ab8263
kmg0205 <mg3709@naver.com>
2023-01-13 10:38:23 +0900

    yes

---
+++ work_java/Java_Big_Data_1230/src/상속기본/BasicInheritance.java
@@ -0,0 +1,12 @@
+package 상속기본;
+
+public class BasicInheritance {
+    public static void main(String[] args) {
+
+        HouseDog housedog = new HouseDog();
+        housedog.setName("happy");
+        housedog.sleep();
+        housedog.sleep(4);
+
+    }
+}
