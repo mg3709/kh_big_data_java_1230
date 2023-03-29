8825485b982b6584b4f8a47b6934e204ea6ca01a
kmg0205 <mg3709@naver.com>
2023-03-29 10:32:52 +0900

    3/29

--- FrontEnd/JAVASCRIPT_AM/객체/배열객체.js
+++ FrontEnd/JAVASCRIPT_AM/객체/배열객체.js
@@ -31,3 +31,52 @@ console.log(fruits2);
 let fruits3 = ["Banana", "Orange", "Apple", "Mango"];
 fruits3[0] = "KIWI";
 console.log(fruits3);
+
+// 2개 이상의 배열을 하나의 뱅렬로 결합 : concat()
+let arr5 = ["사과", "딸기", "참외"];
+let arr6 = ["포도", "수박", "바나나"];
+let arr7 = ["키위", "망고", "오렌지"];
+let arr8 = ["복숭아", "자두", "멜론"];
+let toFruits = arr5.concat(arr6, arr7, arr8);
+console.log(toFruits);
+
+// slice() : 시작 인덱스 부터 종료 인덱스까지의 배열의 요소를 잘라냄
+let fruits10 = ["Banana", "Orange", "Apple", "Mango", "Lemon"];
+let slice1 = fruits10.slice(3); //3번째 요소부터 끝까지
+let slice2 = fruits10.slice(1, 3); //1번 인덱스에서 3번 인덱스 미만 요소
+console.log(slice1);
+console.log(slice2);
+
+// sort() : 오름차순 정렬
+fruits10.sort();
+console.log(fruits10);
+
+// 내림차순 정렬
+console.log(fruits10.reverse());
+
+// filter() : 배열에서 특정 조건을 만족하는 배열의 요소만을 찾아서 새로운 배열생성
+let persons = [
+    {
+    name: "유재석",
+    point: 78,
+    city: "서울",
+    },
+    {
+        name: "김종국",
+        point: 92,
+        city: "서울",
+    },
+    {
+        name: "양세찬",
+        point: 76,
+        city: "제주",
+    },
+    {
+        name: "하하",
+        point: 81,
+        city: "서울",
+    },
+];
+
+let pass = persons.filter(person => person.point > 80);
+console.log(pass);
--- FrontEnd/JAVASCRIPT_AM/객체/평균점수.js
+++ FrontEnd/JAVASCRIPT_AM/객체/평균점수.js
@@ -1,15 +1,17 @@
-// 5명의 성적을 입력 받고, 40점 이하는 보충 학습을 듣는 조건으로 항상 
+// 5명의 성적을 입력 받고, 40점 이하는 보충 학습을 듣는 조건으로 항상 40점
 // 5명의 성적에 대한 평균을 구하기
 
-function averScore(){
+function cal(){
     const scores = [];
+    let total = 0;
 
     for(let i = 0; i < 5; i++){
-        const scroe = parseInt(document.getElementById("score + i").value);
-        score.push(scores);
+        const score = parseInt(document.getElementById("input" + (i + 1)).value);
+        score.push(score);
     }
     for(let i = 0; i < scores.length; i++){
         if(scores[i] < 40) scores[i] = 40;
         total += scores[i];
     }
+    document.getElementById("result").innerHTML = total;
 }
