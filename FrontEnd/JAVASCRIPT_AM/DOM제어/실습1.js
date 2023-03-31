9a90c46f1aba95c00f5b722dcca2293559913595
kmg0205 <mg3709@naver.com>
2023-03-31 10:35:16 +0900

    3/31

--- FrontEnd/JAVASCRIPT_AM/DOM제어/실습1.js
+++ FrontEnd/JAVASCRIPT_AM/DOM제어/실습1.js
@@ -1,8 +1,24 @@
-const uEl = document.getElementById("list");
-let newList = document.createElement("li");
+function newRegister(){
+    let newItem = document.createElement("li"); // ul 태그 아래에 추가할 요소노드 생성
+    let subject = document.querySelector("#subject");
+    // HTML 문서에 새로운 텍스트 노드를 만듭니다
+    let newText = document.createTextNode(subject.value);
+    // 텍스트 노드를 요소 노드의 자식 노드로 추가
+    newItem.appendChild(newText);
 
-console.log(newList);
+    let itemList = document.querySelector("#itemList");
+    // insertBefore() 메소드는 DOM 요소를 특정 요소의 이전에 삽입합니다
+    // 자식 노드 중 첫번재 노드 앞에 추가
+    itemList.insertBefore(newItem, itemList.childNodes[0]);
 
-function add(){
-    uEl.appendChild(newList);
+    subject.value = "";
+    let items = document.querySelectorAll("li");
+
+    for(let e of items){
+        e.addEventListener("click", function(){ 
+            if(this.parentNode){// 클릭 이벤트가 발생한 요소의 부모가 있으면..
+                this.parentNode.removeChild(this);
+            }
+        });
+    }
 }
